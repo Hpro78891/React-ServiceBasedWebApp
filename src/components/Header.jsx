f7317f5e33648ad07af6d2d8aa81a967a9c517dd
Hpro78891 <handsomeprogrammer753@gmail.com>
2022-11-10 22:17:05 +0530

    name change to check netlify update from vscode or not

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import {HashLink} from 'react-router-hash-link'
 const Header = () => {
   return (
     <nav>
-      <h1>Techy star.</h1>
+      <h1>Rishabh.</h1>
       <main>
         <HashLink to={"/#home"}>Home</HashLink>
         <Link to={"/contact"}>Contact</Link>
